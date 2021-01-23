13c13292cb9bcff1279cc70523cbaa1005480ace
Rostroh <romeostroh@gmail.com>
2021-01-23 15:53:59 +0100

    remove useless func

--- srcs/payload.c
+++ srcs/payload.c
@@ -1,26 +1,4 @@
 #include "ft_ping.h"
-/*
-static struct ip	set_ip(struct sockaddr_in dst, int ttl)
-{
-	struct ip		ip_hdr;
-
-	printf("debut setip\n");
-	inet_pton(AF_INET, "10.0.2.15", &ip_hdr.ip_src);
-	printf("after inetpton\n");
-	ft_memcpy(&ip_hdr.ip_dst, &dst.sin_addr, sizeof(struct in_addr));
-	printf("after memcpy\n");
-	ip_hdr.ip_v = 4;
-	ip_hdr.ip_hl = 5;
-	ip_hdr.ip_tos = 0;
-	ip_hdr.ip_len = 56;
-	ip_hdr.ip_id = 321;
-	ip_hdr.ip_off = 0;
-	ip_hdr.ip_ttl = ttl;
-	ip_hdr.ip_p = IPPROTO_ICMP;
-	ip_hdr.ip_sum = 0;
-	printf("after init\n");
-	return (ip_hdr);
-}*/
 
 char				*creat_payload(struct sockaddr_in dst, t_info data)
 {
